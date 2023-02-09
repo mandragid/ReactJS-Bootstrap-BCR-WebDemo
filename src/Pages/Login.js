f7881e4980c6f9a10c71f279ed93877e13b4db8f
Mandragid <mandragid@MacBook-Pro-Macbook.local>
2023-02-09 20:32:17 +0700

    setup api getcars

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -6,61 +6,64 @@ import { userAction } from "../Redux/userAction";
 import { useEffect } from "react";
 
 const Login = () => {
-	const dispatch = useDispatch();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-	const handleEmail = (e) => {
-		setEmail(e.target.value);
-	};
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-	const handlePassword = (e) => {
-		setPassword(e.target.value);
-	};
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-	const handleLogin = () => {
-		const payLoad = {
-			email: email,
-			password: password,
-		};
-		dispatch(userAction(payLoad));
-	};
+  const handleLogin = async () => {
+    const payLoad = {
+      email: email,
+      password: password,
+    };
+    await dispatch(userAction(payLoad));
 
-	useEffect(() => {
-		handleRedirect();
-	});
+    setTimeout(() => {
+      navigate("/admin/dashboard");
+    }, 2000);
+  };
 
-	const handleRedirect = () => {
-		const token = localStorage.getItem("token");
-		setTimeout(() => {
-			if (!!token) {
-				navigate("/admin/dashboard");
-			}
-		}, 2000);
-	};
+  //
 
-	return (
-		<div>
-			<NavBar />
-			{
-				<div className="register-section">
-					<div>
-						<h1>Login Admin</h1>
-					</div>
-					<div className="register-input-bg">
-						<input onChange={handleEmail} placeholder="input email" type="email" className="register-input" />
-						<input onChange={handlePassword} placeholder="input password" type="password" className="register-input" />
-					</div>
-					<div className="register-button-bg">
-						<button onClick={handleLogin} className="register-button">
-							login
-						</button>
-					</div>
-				</div>
-			}
-		</div>
-	);
+  return (
+    <div>
+      <NavBar />
+      {
+        <div className="register-section">
+          <div>
+            <h1>Login Admin</h1>
+          </div>
+          <div className="register-input-bg">
+            <input
+              onChange={handleEmail}
+              placeholder="input email"
+              type="email"
+              className="register-input"
+            />
+            <input
+              onChange={handlePassword}
+              placeholder="input password"
+              type="password"
+              className="register-input"
+            />
+          </div>
+          <div className="register-button-bg">
+            <button onClick={handleLogin} className="register-button">
+              login
+            </button>
+          </div>
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Login;
--- src/const/endpoint.js
+++ src/const/endpoint.js
@@ -1,6 +1,8 @@
 export const API = {
-	REGISTER: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/register",
-	LOGIN: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/login",
-	GET_ORDERLIST: "https://bootcamp-rent-cars.herokuapp.com/admin/v2/order?sort=created_at%3Adesc&page=1&pageSize=10",
-	GET_ADMIN_CARS: "https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?name=0&page=0&pageSize=0",
+  REGISTER: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/register",
+  LOGIN: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/login",
+  GET_ORDERLIST:
+    "https://bootcamp-rent-cars.herokuapp.com/admin/v2/order?sort=created_at%3Adesc&page=1&pageSize=10",
+  GET_ADMIN_CARS:
+    "https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?page=1&pageSize=10",
 };

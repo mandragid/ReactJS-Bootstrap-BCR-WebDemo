27339c8a475954ab550148720847f78f5c9f79f8
gumgum <ronaldovolkert@gmail.com>
2023-02-09 12:10:16 +0700

    Authentiication Customer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import SearchResult from "./Pages/SearchResult";
 import CarDetail from "./Pages/CarDetail";
 import LandingPage from "./Pages/LandingPage";
 import Dashboard from "./admin/Pages/Dashboard";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
+import Login from "./Pages/LoginAdmin";
+import Register from "./Pages/RegisterAdmin";
+import RegisterCustomer from "./Pages/RegisterCustomer";
+import LoginCustomer from "./Pages/LoginCustomer";
 
 function App() {
 	return (
@@ -20,6 +22,8 @@ function App() {
 					<Route path="/admin/dashboard" element={<Dashboard />} />
 					<Route path='/admin/Login' element = {<Login/>} />
       				<Route path='/admin/Register' element = {<Register/>} />
+					<Route path='/Login' element = {<LoginCustomer/>} />
+      				<Route path='/Register' element = {<RegisterCustomer/>} />
 				</Routes>
 			</BrowserRouter>
 		</div>
--- src/const/endpoint.js
+++ src/const/endpoint.js
@@ -1,4 +1,6 @@
 export const API = {
     REGISTER: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/register",
     LOGIN: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/login",
+    REGISTER_CUSTOMER: "https://bootcamp-rent-cars.herokuapp.com/customer/auth/register",
+    LOGIN_CUSTOMER: "https://bootcamp-rent-cars.herokuapp.com/customer/auth/login"
 }

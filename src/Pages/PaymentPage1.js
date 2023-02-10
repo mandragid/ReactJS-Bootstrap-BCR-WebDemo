9bfa9868d29aa10236db27408d90e5435035b10c
luthfianesa <lutvheanesa@gmail.com>
2023-02-10 20:38:48 +0700

    Add navbar and  paymentStep1 to PaymentPage1

--- src/Pages/PaymentPage1.js
+++ src/Pages/PaymentPage1.js
@@ -1,8 +1,13 @@
 import "../Pages/pages.css";
+import NavBar from "../components/NavBar"
+import PaymentStep1 from "../components/PaymentStep1";
 
 const PaymentPage1 = () => {
   return (
     <div>
+      <NavBar />
+      <div className="empty-header"></div>
+      <PaymentStep1 />
     </div>
   );
 };

ef84c8193ea2eb5757076d83cf3b9f4cf9c4cb57
luthfianesa <lutvheanesa@gmail.com>
2023-02-16 12:11:11 +0700

    Fix Endpoint

--- src/const/endpoint.js
+++ src/const/endpoint.js
@@ -1,8 +1,12 @@
 export const API = {
   REGISTER: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/register",
   LOGIN: "https://bootcamp-rent-cars.herokuapp.com/admin/auth/login",
-  GET_ORDERLIST:
-    "https://bootcamp-rent-cars.herokuapp.com/admin/v2/order?sort=created_at%3Adesc&page=1&pageSize=10",
-  GET_ADMIN_CARS:
-    "https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?page=1&pageSize=10",
+  // GET_ORDERLIST: "https://bootcamp-rent-cars.herokuapp.com/admin/v2/order?sort=created_at%3Adesc&page=1&pageSize=10",
+  // GET_ADMIN_CARS: "https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?page=1&pageSize=10",
+  POST_CUSTOMER_ORDER: `https://bootcamp-rent-cars.herokuapp.com/customer/order`,
+  // Add Car id on GET_CUSTOMER_ORDER
+  GET_CUSTOMER_ORDER: `https://bootcamp-rent-cars.herokuapp.com/customer/order/`,
+  GET_ORDERLIST: "https://bootcamp-rent-cars.herokuapp.com/admin/v2/order?page=1&pageSize=10",
+  GET_ADMIN_CARS: "https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?page=1&pageSize=10",
+  POST_ADMIN_CAR: "https://bootcamp-rent-cars.herokuapp.com/admin/car",
 };

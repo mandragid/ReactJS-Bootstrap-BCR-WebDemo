9b08fee34a4e8afc20a04ed4e4f0dc598a1be3b7
gumgum <ronaldovolkert@gmail.com>
2023-02-18 03:15:24 +0700

    added second protected route

--- src/App.js
+++ src/App.js
@@ -15,25 +15,29 @@ import CarList from "./admin/Pages/CarList";
 import ProtectedRouteAdmin from "./hoc/ProtectedRouteAdmin";
 import AdminAddCar from "./admin/Pages/AdminAddCar";
 import AdminEditCar from "./admin/Pages/AdminEditCar";
+import ProtectedRoute from "./hoc/ProtectedRouteCustomer";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/cari-mobil" element={<SearchCar />} />
-          <Route path="/hasil-pencarian" element={<SearchResult />} />
-          <Route path="/detail-paket/:id" element={<CarDetail />} />
-          <Route path="/payment1/:id" element={<PaymentPage1 />} />
-          <Route path="/payment2/:id/" element={<PaymentPage2 />} />
           <Route path="/Login" element={<LoginCustomer />} />
           <Route path="/Register" element={<RegisterCustomer />} />
-          <Route element={<ProtectedRouteAdmin />}>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/cari-mobil" element={<SearchCar />} />
+            <Route path="/hasil-pencarian" element={<SearchResult />} />
+            <Route path="/detail-paket/:id" element={<CarDetail />} />
+            <Route path="/payment1/:id" element={<PaymentPage1 />} />
+            <Route path="/payment2/:id/" element={<PaymentPage2 />} />
+            </Route>
+            <Route element={<ProtectedRouteAdmin />}>
             <Route path="/admin/cars" element={<CarList />} />
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/addcar" element={<AdminAddCar />} />
             <Route path="/admin/edit-car/:id" element={<AdminEditCar />} />
-          </Route>
+            </Route>
+          
           <Route path="/admin/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
---
+++ src/hoc/ProtectedRouteCustomer.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { AuthCheck } from "../Redux/authAction";
+
+const ProtectedRoute = () => {
+  const { authReducer } = useSelector((rootReducer) => rootReducer);
+  const dispatch = useDispatch();
+  console.log("auth", authReducer);
+  //part 1
+
+  useEffect(() => {
+    dispatch(AuthCheck());
+  }, [authReducer.isLogin]);
+
+  //part 2
+  if (authReducer.loading) {
+    return "loading...";
+  }
+
+  //part3
+  console.log("hasil", authReducer.isLogin);
+  return authReducer.isLogin ? <Outlet /> : <Navigate to={"/Login"} />;
+};
+
+export default ProtectedRoute;
